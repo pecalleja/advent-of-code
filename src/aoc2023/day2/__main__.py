31566d1813814ed342584464293a5003691639bc
Pedro Calleja <pedro.calleja.-nd@disney.com>
2023-12-04 11:54:15 -0500

    2023 refactor day 2

---
+++ src/aoc2023/day2/__main__.py
@@ -0,0 +1,9 @@
+from .solution import Part1Solution
+from .solution import Part2Solution
+
+
+part1 = Part1Solution.from_file()
+part2 = Part2Solution.from_file()
+print(
+    f"The result for day2 is part1: {part1.result()}, part2: {part2.result()}"
+)
--- src/aoc2023/day2/solution.py
+++ src/aoc2023/day2/solution.py
@@ -1,46 +1,59 @@
-import os
 import re
+from functools import reduce
+
+from aoc2023 import Solution
 
 
-class Solution:
+class Day2Solution(Solution):
     limits = {
         "red": 12,
         "green": 13,
         "blue": 14,
     }
 
-    def __init__(self, data_input):
-        self.data = data_input
-
     def result(self):
+        raise NotImplementedError
+
+    def parsed_game(self):
         pattern = re.compile(r"\d+\sred|\d+\sgreen|\d+\sblue")
-        result = 0
         for line in self.data:
             game_id, game_result = line.split(":")
             game_id = game_id.split()[-1]
             game_id = int(game_id)
             game_result = game_result.strip()
-            values = {
+            for match in pattern.findall(game_result):
+                value, color = match.split()
+                yield game_id, color, int(value)
+
+
+class Part1Solution(Day2Solution):
+    def result(self):
+        games = {i: True for i in range(1, len(self.data) + 1)}
+        for game_id, color, value in self.parsed_game():
+            if self.limits[color] < value:
+                games[game_id] = False
+        result = sum(
+            [game_id for game_id, valid in games.items() if valid is True]
+        )
+        return result
+
+
+class Part2Solution(Day2Solution):
+    def result(self):
+        games = {
+            i: {
                 "red": [],
                 "green": [],
                 "blue": [],
             }
-            for match in pattern.findall(game_result):
-                value, color = match.split()
-                values[color].append(int(value))
-            multiplication = 1
+            for i in range(1, len(self.data) + 1)
+        }
+        for game_id, color, value in self.parsed_game():
+            games[game_id][color].append(value)
+        result = 0
+        for game_id, values in games.items():
             for color, elements in values.items():
                 values[color] = max(elements)
-                multiplication *= values[color]
-            print(f"game {game_id} values: {values}")
+            multiplication = reduce(lambda x, y: x * y, values.values())
             result += multiplication
         return result
-
-
-if __name__ == "__main__":
-    this_dir = os.path.dirname(__file__)
-    full_path = os.path.join(this_dir, "input.txt")
-    with open(full_path) as file_input:
-        data = file_input.readlines()
-        result = Solution(data).result()
-        print(f"The result for input.txt is: {result}")
